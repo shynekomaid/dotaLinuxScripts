00a7e8f6cad7244a1f3d24217c89ae6bc96612a4
tminei <34548743+tminei@users.noreply.github.com>
2020-05-05 02:55:43 +0300

    Add files via upload

---
+++ Sky/Procast.py
@@ -0,0 +1,32 @@
+atos = "c"
+discord = "x"
+eth = "v"
+q = "q"
+w = "w"
+e = "e"
+r = "r"
+
+s = "s"
+
+keyboard.send_key(atos)
+keyboard.send_key(atos)
+keyboard.send_key(discord)
+keyboard.send_key(discord)
+keyboard.send_key(e)
+time.sleep(0.165)
+keyboard.send_key(eth)
+keyboard.send_key(eth)
+keyboard.send_key(eth)
+keyboard.send_key(eth)
+keyboard.send_key(r)
+time.sleep(0.165)
+keyboard.send_key(w)
+keyboard.send_key(w)
+keyboard.send_key(w)
+keyboard.send_key(w)
+keyboard.send_key(q)
+keyboard.send_key(q)
+keyboard.send_key(q)
+keyboard.send_key(q)
+
+
